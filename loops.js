2e4dee9198780c7335cedc29724aec75fef0021d
student <audreymunyiva@gmail.com>
2021-04-05 08:58:09 +0300

    first commit

---
+++ loops.js
@@ -0,0 +1,15 @@
+const names=['Audrey','Jocelyne','Davies','Patrick','Marie','Harry','Potter'];
+
+for(Name of names){
+    console.log(Name);
+}
+
+var countries={"Canada":"Otowa","Kenya":"Nairobi","Nigeria":"Lagos"};
+
+for(let country in countries){
+    
+    console.log(country);
+
+}
+ 
+
